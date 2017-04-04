7b6534d11aa2e3d7ba169db5a6322aa0ffb49e8c
Minea Chem <minea@mineas-Mac.local>
2017-04-04 10:53:49 +0700

    first commit

---
+++ BirthdayCoreDataDemo/CoreDataStack.swift
@@ -0,0 +1,9 @@
+//
+//  CoreDataStack.swift
+//  BirthdayCoreDataDemo
+//
+//  Created by Minea Chem on 3/27/17.
+//  Copyright © 2017 Minea Chem. All rights reserved.
+//
+
+import Foundation
---
+++ BirthdayCoreDataDemo/Friend.swift
@@ -0,0 +1,9 @@
+//
+//  Friend.swift
+//  BirthdayCoreDataDemo
+//
+//  Created by Minea Chem on 3/29/17.
+//  Copyright © 2017 Minea Chem. All rights reserved.
+//
+
+import Foundation
